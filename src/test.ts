e2fcf4098cfe7cb7703476cc56fb680de97c20aa
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2023-01-26 13:05:22 +0000

    Added tests for existing functionality

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,19 @@
 import { calculateEditDistance, wordErrorRate } from "./app";
+import {describe, expect, it} from '@jest/globals'
 
-console.log(calculateEditDistance("one hen", "won hen")); // => 1
-console.log(wordErrorRate("one hen", "won hen")); // => 0.5
-
-console.log(calculateEditDistance("one hen two ducks", "won hen too ducts")); // => 3
-console.log(wordErrorRate("one hen two ducks", "won hen too ducts")); // => 0.25
+describe('default behavior', () => {
+  it('calculateDistance', () => {
+    expect(calculateEditDistance('one hen', 'won hen')).toEqual(1)
+    expect(calculateEditDistance('one hen two docks', 'won hen too ducts')).toEqual(3)
+    expect(calculateEditDistance('i like babies', 'i am like babies')).toEqual(1) // one insertion
+    expect(calculateEditDistance('i will not go', 'i will go')).toEqual(1) // one deletion
+  })
+  
+  it('wordErrorRate', () => {
+    expect(wordErrorRate('one hen', 'won hen')).toEqual(0.5)
+    expect(wordErrorRate('one hen two docks', 'won hen too ducts')).toEqual(0.75)
+    // calculated with respect to the longer input
+    expect(wordErrorRate('i like babies', 'i am like babies')).toEqual(0.25) 
+    expect(wordErrorRate('i will not go', 'i will go')).toEqual(0.25) // one deletion
+  })
+})
